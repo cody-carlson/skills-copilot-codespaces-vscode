cafa56f5e3b6972e5969bbdb7f842fefd8d408a4
cody-carlson <78826051+cody-carlson@users.noreply.github.com>
2023-07-20 23:02:57 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var memberActive = document.getElementById("memberActive");
+    var skillsActive = document.getElementById("skillsActive");
+    member.style.display = "block";
+    skills.style.display = "none";
+    memberActive.style.display = "block";
+    skillsActive.style.display = "none";
+}
